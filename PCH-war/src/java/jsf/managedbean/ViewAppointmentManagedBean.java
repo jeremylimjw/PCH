e6f38d911c2a940ee9290886f9fdc2b222863f84
NgJunKai <e0407444@u.nus.edu>
2021-03-25 16:33:11 +0800

    attempting to add viewMC in viewAppt.xhtml

--- PCH-war/src/java/jsf/managedbean/ViewAppointmentManagedBean.java
+++ PCH-war/src/java/jsf/managedbean/ViewAppointmentManagedBean.java
@@ -8,13 +8,26 @@ package jsf.managedbean;
 import ejb.session.stateless.AppointmentSessionBeanLocal;
 import entity.Appointment;
 import entity.Prescription;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.sql.DataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperRunManager;
 import util.enumeration.AppointmentTypeEnum;
 import util.exception.AppointmentEntityException;
 
@@ -23,36 +36,46 @@ import util.exception.AppointmentEntityException;
  * @author USER
  */
 @Named(value = "viewAppointmentManagedBean")
-@RequestScoped
-public class ViewAppointmentManagedBean {
+@ViewScoped
+public class ViewAppointmentManagedBean implements Serializable {
+
+    @Resource(name = "pchDataSource")
+    private DataSource pchDataSource;
 
     @EJB(name = "AppointmentSessionBeanLocal")
     private AppointmentSessionBeanLocal appointmentSessionBeanLocal;
-    
+
     private BigDecimal basicRate = new BigDecimal(40);
     private Appointment appointment;
-    
+    private Long duration;
+
     public ViewAppointmentManagedBean() {
     }
-    
+
     @PostConstruct
     public void postConstruct() {
         try {
+
             Long id = Long.parseLong(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("id"));
             appointment = appointmentSessionBeanLocal.retrieveById(id);
-            
-            if (appointment.getSchedule_type().equals(AppointmentTypeEnum.CONSULTATION)) basicRate = new BigDecimal(40);
-            else if (appointment.getSchedule_type().equals(AppointmentTypeEnum.HEALTH_CHECKUP)) basicRate = new BigDecimal(20);
-            else if (appointment.getSchedule_type().equals(AppointmentTypeEnum.VACCINATION)) basicRate = new BigDecimal(10);
-            else  basicRate = new BigDecimal(40);
-            
+
+            if (appointment.getSchedule_type().equals(AppointmentTypeEnum.CONSULTATION)) {
+                basicRate = new BigDecimal(40);
+            } else if (appointment.getSchedule_type().equals(AppointmentTypeEnum.HEALTH_CHECKUP)) {
+                basicRate = new BigDecimal(20);
+            } else if (appointment.getSchedule_type().equals(AppointmentTypeEnum.VACCINATION)) {
+                basicRate = new BigDecimal(10);
+            } else {
+                basicRate = new BigDecimal(40);
+            }
+
             calculateTotal();
-            
-        } catch (NumberFormatException | AppointmentEntityException ex) { 
+
+        } catch (NumberFormatException | AppointmentEntityException ex) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), null));
         }
     }
-    
+
     public void calculateTotal() {
         BigDecimal total = new BigDecimal(0);
         for (Prescription p : appointment.getPrescriptions()) {
@@ -60,9 +83,47 @@ public class ViewAppointmentManagedBean {
         }
         appointment.setTotal_price(total.add(basicRate));
     }
-    
-    public void viewMc() {
+
+    public void viewMc(ActionEvent event) {
 //        --- View MC Logic here ---
+
+        try {
+            duration = appointment.getMedical_certificate().getEnd_date().getTime() - appointment.getMedical_certificate().getStart_date().getTime();
+            long diff = TimeUnit.MILLISECONDS.toDays(duration) + 1;
+            HashMap parameters = new HashMap();
+            parameters.put("Title", "Medical Certificate");
+            parameters.put("INPUT_ID", appointment.getId());
+            parameters.put("INPUT_MedID", appointment.getMedical_certificate().getId());
+            parameters.put("Duration", "For the duration of");
+            parameters.put("INPUT_NAME", diff);
+            parameters.put("Message", "This is to certify that Mr/Ms :  ");
+            parameters.put("dr ", "Doctor");
+            parameters.put("line", "__________________");
+            parameters.put("header", "____________________________________________________________________________________________________");
+            parameters.put("patient", appointment.getMedical_record().getName());
+            parameters.put("from", "Was examined and treated at PCH and would need futher medical attention");
+            parameters.put("FROM", "From ");
+            parameters.put("Date", "Date: ");
+            parameters.put("Day", "Day(s) ");
+            parameters.put("to", "to");
+            parameters.put("line1", "______________________________________");
+            parameters.put("line2", "________________________");
+            parameters.put("line3", "_________________________");
+            parameters.put("line4", "_________________________");
+
+            parameters.put("Dr_name", appointment.getEmployee().getName());
+            InputStream reportStream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("/jasperreport/medicalcert.jasper");
+            OutputStream outputStream = FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream();
+
+            JasperRunManager.runReportToPdfStream(reportStream, outputStream, parameters, pchDataSource.getConnection());
+
+        } catch (JRException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+        }
+
     }
 
     public Appointment getAppointment() {
@@ -80,5 +141,19 @@ public class ViewAppointmentManagedBean {
     public void setBasicRate(BigDecimal basicRate) {
         this.basicRate = basicRate;
     }
-    
+
+    /**
+     * @return the duration
+     */
+    public Long getDuration() {
+        return duration;
+    }
+
+    /**
+     * @param duration the duration to set
+     */
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
 }
